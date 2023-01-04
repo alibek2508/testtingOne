8b9423df73a65f11d117e29b77e26398f5b57623
alibek2508 <a.tukhbayev@yandex.ru>
2023-01-04 14:06:44 +0600

    add movia films

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-"use strict";
+//Задания 1
+// let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+// console.log(`${numberOfFilms} фильмов`);
+
+//Задания 2
+let personalMovieDB = {
+    count: prompt('Страна'),
+    movies: prompt('Фильм'),
+    actors: prompt('Актер'),
+    genres: prompt([]),
+    privat: prompt(true)
+};
+console.log(personalMovieDB);
