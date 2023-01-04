d797c6eee855808a897a47a9465ae419748e900e
alibek2508 <a.tukhbayev@yandex.ru>
2023-01-04 21:51:55 +0600

    add movia films 1

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,19 @@
 // console.log(`${numberOfFilms} фильмов`);
 
 //Задания 2
-let personalMovieDB = {
-    count: prompt('Страна'),
-    movies: prompt('Фильм'),
-    actors: prompt('Актер'),
-    genres: prompt([]),
-    privat: prompt(true)
-};
-console.log(personalMovieDB);
+// let personalMovieDB = {
+//     count: prompt('Страна'),
+//     movies: prompt('Фильм'),
+//     actors: prompt('Актер'),
+//     genres: prompt([]),
+//     privat: prompt(true)
+// };
+// console.log(personalMovieDB);
+let depositValiy = 12000;
+let depositProcent = (depositValiy / 100 * 7) * 2;
+let tolalPay = depositValiy + depositProcent;
+if (tolalPay > 13500) {
+    console.log('Васли сможет купить квартиру');
+} else {
+    console.log('Не сможет купить');
+}
